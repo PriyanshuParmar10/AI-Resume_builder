6021dc56f16e04266cb8883de597e41d5d431c19
priyanshuparmar10 <priyanshuparmar2022@gmail.com>
2026-06-20 16:54:18 +0530

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://ai-resume-builder-zeta-khaki.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
